d616fdcb4082a9b22b25049b75825368601bee71
PabloSz <pablosaez1995@gmail.com>
2020-05-23 19:35:06 -0400

    only basic

--- pages/api/clearPosts.js
+++
@@ -1,7 +0,0 @@
-import { db } from "../../src/api/db";
-
-export default (req, res) => {
-  db.get("posts").remove().write();
-
-  res.send(db.get("posts").value());
-};
--- src/api/db.js
+++
@@ -1,8 +0,0 @@
-import LowDb from "lowdb";
-import FileSync from "lowdb/adapters/FileSync";
-
-export const db = LowDb(new FileSync("./db.json"));
-
-db.defaults({
-  posts: [],
-}).write();
